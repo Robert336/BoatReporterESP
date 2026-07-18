bf657197592c761446dd14fd0a1a5dd13d1409ea
Robert336 <Robert336@users.noreply.github.com>
2026-07-18 19:52:33 -0400

    release: bump firmware version to 1.1.4

--- include/Version.h
+++ include/Version.h
@@ -2,7 +2,7 @@
 
 // Firmware version - update this for each release
 // Format: "major.minor.patch"
-#define FIRMWARE_VERSION "1.1.3"
+#define FIRMWARE_VERSION "1.1.4"
 
 // Build information (automatically set by build flags if needed)
 #ifndef BUILD_TIMESTAMP
