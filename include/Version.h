53c7b35ed10da13feb1209ea5926b0a0a65ef09c
Robert336 <Robert336@users.noreply.github.com>
2026-07-18 21:18:00 -0400

    Bump firmware version to 1.1.5

--- include/Version.h
+++ include/Version.h
@@ -2,7 +2,7 @@
 
 // Firmware version - update this for each release
 // Format: "major.minor.patch"
-#define FIRMWARE_VERSION "1.1.5-dev"
+#define FIRMWARE_VERSION "1.1.5"
 
 // Build information (automatically set by build flags if needed)
 #ifndef BUILD_TIMESTAMP
